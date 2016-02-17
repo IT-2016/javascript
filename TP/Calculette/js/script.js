d591f3d587b0b4ee46459379dd9c199efe21c86c
sferey <s.ferey@cleo-consulting.fr>
2016-02-17 12:26:41 +0100

    Update script.js

--- TP/Calculette/js/script.js
+++ TP/Calculette/js/script.js
@@ -2,11 +2,17 @@ setInterval(function(){
 	if($("#display div").css("color") == "rgb(255, 255, 0)")
 		$("#display div").css("color", "black")
 	else 
-		$("#display div").css("color", "yellow")			
+		$("#display div").css("color", "yellow")
 },500);
-
 $("#pad button").on('click', function(){
-	if($("#display div").text() == "0")
+	if($("#display div").text() == "0"){
 		$("#display div").text("");
-	$("#display div").append($(this).text());
+	}
+	
+	if($(this).hasClass("btn-danger") === true) {
+		var result = eval($("#display div").text()); 
+		$("#display div").text(result);
+	} else {
+		$("#display div").append($(this).text());
+	}
 });
